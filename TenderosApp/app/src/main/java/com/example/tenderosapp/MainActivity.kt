5a65fcf2311e4b66e6aa4444062943ea81e4398c
Esteban Arocha <estebanarocha1@gmail.com>
2020-03-29 20:37:07 -0600

    Test read qr code in main activity

--- TenderosApp/app/src/main/java/com/example/tenderosapp/MainActivity.kt
+++ TenderosApp/app/src/main/java/com/example/tenderosapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.tenderosapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +17,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         navController = findNavController(R.id.nav_host_fragment)
 
+        val integrator = IntentIntegrator(this)
+        //integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE) // optional
+        integrator.initiateScan()
+    }
 
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        val result =
+            IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            val value = result.contents
+            if (value != null) {
+                Log.d("Holi", value)
+            } else {
+                Log.d("holi", "failed")
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
--- TenderosApp/app/src/main/java/com/example/tenderosapp/ui/transaction/TransactionFragment.kt
+++ TenderosApp/app/src/main/java/com/example/tenderosapp/ui/transaction/TransactionFragment.kt
@@ -2,22 +2,28 @@ package com.example.tenderosapp.ui.transaction
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
 import com.example.tenderosapp.R
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.fragment_login.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
-class TransactionFragment : Fragment() {
+class TransactionFragment : Fragment(R.layout.fragment_transaction) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_transaction, container, false)
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.fragment_transaction, container, false)
+//    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        tittle_tv.text = "Hello"
+
     }
 
 }
