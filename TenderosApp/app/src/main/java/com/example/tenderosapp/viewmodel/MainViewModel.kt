1735ca81e000c6adb013bcc77764b54d7f146fe3
alextrujillo <alexandro4v@gmail.com>
2020-04-25 11:20:41 -0500

    Mainviewmodel added

--- TenderosApp/app/src/main/java/com/example/tenderosapp/viewmodel/MainViewModel.kt
+++ TenderosApp/app/src/main/java/com/example/tenderosapp/viewmodel/MainViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.tenderosapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    enum class AuthenticationState {
+        UNAUTHENTICATED,        // Initial state, the user needs to authenticate
+        AUTHENTICATED  ,
+        INVALID_AUTHENTICATION// The user has authenticated successfully
+    }
+    val authenticationState = MutableLiveData<AuthenticationState>()
+
+    //Intance
+    init {
+        authenticationState.value = AuthenticationState.UNAUTHENTICATED
+    }
 }
